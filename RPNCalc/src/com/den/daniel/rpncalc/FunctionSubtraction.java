4a8713cc50334442d39186950aef3650a0cffb52
Bugamn <bugamn@den>
2013-01-20 12:30:36 -0200

    Added subtraction.

---
+++ RPNCalc/src/com/den/daniel/rpncalc/FunctionSubtraction.java
@@ -0,0 +1,21 @@
+package com.den.daniel.rpncalc;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+import android.content.res.Resources;
+
+public class FunctionSubtraction extends Function {
+
+	public FunctionSubtraction(Stack<Double> stack, Resources resources) {
+		super(stack, resources, 2);
+	}
+
+	@Override
+	protected ArrayList<Double> operate(ArrayList<Double> args) {
+		ArrayList<Double> result = new ArrayList<Double>();
+		result.add(args.get(0) - args.get(1));
+		return result;
+	}
+
+}
